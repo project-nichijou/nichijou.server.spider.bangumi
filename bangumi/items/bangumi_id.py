86de836ddb8970095301c4f9c402079bf7996c92
JeffersonQin <1247006353@qq.com>
2021-06-22 00:00:34 +0800

    [Add] annotation for bangumi id item

--- bangumi/items/bangumi_id.py
+++ bangumi/items/bangumi_id.py
@@ -2,7 +2,11 @@ import scrapy
 
 
 class BangumiIDItem(scrapy.Item):
-	id = scrapy.Field()
+	# subject id
+	sid = scrapy.Field()
+	# type of subject
 	type = scrapy.Field()
+	# 原名
 	name = scrapy.Field()
+	# 中文名 (没有就是原名)
 	cn_name = scrapy.Field()
