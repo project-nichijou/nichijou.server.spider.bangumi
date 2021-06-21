8b6eb212db1135f294f051d8507d16ef84e0809e
JeffersonQin <1247006353@qq.com>
2021-06-22 00:00:54 +0800

    [Update] intro -> introHTML

--- bangumi/items/bangumi_anime.py
+++ bangumi/items/bangumi_anime.py
@@ -8,8 +8,8 @@ class BangumiAnimeItem(scrapy.Item):
 	name = scrapy.Field()
 	# 中文名 (没有就是原名)
 	cn_name = scrapy.Field()
-	# 简介
-	intro = scrapy.Field()
+	# 简介 (HTML)
+	introHTML = scrapy.Field()
 	# 话数
 	episode = scrapy.Field()
 	# 放送开始
