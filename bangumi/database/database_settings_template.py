a8848daa3a3a61c31ac456e31da8c27b15a3fd28
JeffersonQin <1247006353@qq.com>
2021-06-22 10:29:07 +0800

    [Modify] yaml config -> py config

---
+++ bangumi/database/database_settings_template.py
@@ -0,0 +1,8 @@
+
+CONFIG = {
+	'host': 'your_host',
+	'user': 'your_user',
+	'password': 'your_password',
+	'port': 3306,
+	'database': 'bangumi'
+}
--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 from bangumi.database.bangumi_database import BangumiDatabase
-
-import os
-import yaml
-
-root_dir = os.path.split(os.path.abspath(__file__))[0]
-config_dir = os.path.join(root_dir, 'config.yml')
-
-def read_conf(conf_dir):
-	# Read configuration
-	with open(conf_dir, 'r', encoding='utf8') as f:
-		contents = f.read()
-		return yaml.load(contents, Loader=yaml.FullLoader)
+from bangumi.database import database_settings
 
 if __name__ == '__main__':
-	db = BangumiDatabase(read_conf(config_dir)['database'])
+	db = BangumiDatabase(database_settings.CONFIG)
+	db.write('bangumi_id', {
+		'sid': 296367,
+		'type': 'anime',
+		'name': 'SSSS.DYNAZENON',
+		'cn_name': 'SSSS.电光机王'
+	})
