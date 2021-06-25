0d291997121f8b91779db150a09b7c1e7beb2460
JeffersonQin <1247006353@qq.com>
2021-06-25 22:45:37 +0800

    [Add] cli for spider

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from bangumi.database.bangumi_database import BangumiDatabase
-from bangumi.database import database_settings
+from scrapy import cmdline
+import click
+
+
+@click.group()
+def cli():
+	pass
+
+
+@cli.command()
+@click.argument('spider')
+@click.option('--fail', is_flag=True, default=False, help='whether start in fail mode')
+@click.option('--full', is_flag=True, default=False, help='this option is only for episode introduction, whether crawl the full list. (extremely time consuming)')
+def crawl(spider: str, fail: bool, full: bool):
+	'''
+	start SPIDER crawling using scrapy
+
+	SPIDER: name of the spider to start
+	'''
+	command = f'scrapy crawl {spider}'
+	if fail or full:
+		command = f'{command} -a'
+	if fail:
+		command = f'{command} fail=on'
+	if full:
+		command = f'{command} full=on'
+	cmdline.execute(command.split(' '))
+
 
 if __name__ == '__main__':
-	db = BangumiDatabase(database_settings.CONFIG)
-	db.write('bangumi_id', {
-		'sid': 296367,
-		'type': 'anime',
-		'name': 'SSSS.DYNAZENON',
-		'cn_name': 'SSSS.电光机王'
-	})
+	cli()
