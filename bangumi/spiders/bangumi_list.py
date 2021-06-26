97900d995b96e7651efd6263c316095963bb6a58
DaivdYuan <47169162+DaivdYuan@users.noreply.github.com>
2021-06-26 13:29:19 +0800

    improved request queue

--- bangumi/spiders/bangumi_list.py
+++ bangumi/spiders/bangumi_list.py
@@ -1,20 +1,36 @@
 from bangumi import bangumi_settings
 from bangumi.items.bangumi_id import BangumiIDItem
 import scrapy
+from lxml import etree
+import re
+import requests
 
 
 class BangumiListSpider(scrapy.Spider):
 	name = 'bangumi_list'
 	allowed_domains = [bangumi_settings.BASE_DOMAIN]
 	start_page = 1
+	last_page = 1
 
 	def __init__(self, **kwargs):
 		self.update_properties()
 		super().__init__(**kwargs)
 	
+	def get_last_page(self):
+		cur_url = self.get_current_url()
+		first_page_html = requests.get(cur_url,headers=bangumi_settings.HEADERS).content.decode('utf-8')
+		self.last_page=int(re.findall('page=[0-9][0-9]*',first_page_html)[-1][5:])
+		#print("last_page:",self.last_page)
+	
 	def update_properties(self):
 		self.page_var = self.start_page
-		self.start_urls = [self.get_current_url()]
+		self.get_last_page()
+		self.start_urls = [self.get_url_from_params(_page=pg) for pg in range(1,self.last_page+1)]
+
+	def get_url_from_params(self,_page=None,_type=None):
+		_type = self.type if not _type else _type
+		_page = self.page_var if not _page else _page
+		return f'{bangumi_settings.BASE_URL}/{_type}/browser/?sort=title&page={_page}'
 
 	def get_current_url(self):
 		return f'{bangumi_settings.BASE_URL}/{self.type}/browser/?sort=title&page={self.page_var}'
@@ -34,5 +50,5 @@ class BangumiListSpider(scrapy.Spider):
 			result['name'] = item.xpath('./div/h3/small[@class="grey"]/text()').get()
 			if result['name'] == None: result['name'] = result['cn_name']
 			yield result
-		self.page_var += 1
-		yield scrapy.Request(self.get_current_url())
+		#self.page_var += 1
+		#yield scrapy.Request(self.get_current_url())
