8e7d0e6a1d8700c4938e1363cc7bd894ce5a2285
DaivdYuan <47169162+DaivdYuan@users.noreply.github.com>
2021-06-26 13:39:54 +0800

    improved robustness

--- bangumi/spiders/bangumi_list.py
+++ bangumi/spiders/bangumi_list.py
@@ -1,7 +1,6 @@
 from bangumi import bangumi_settings
 from bangumi.items.bangumi_id import BangumiIDItem
 import scrapy
-from lxml import etree
 import re
 import requests
 
@@ -10,7 +9,7 @@ class BangumiListSpider(scrapy.Spider):
 	name = 'bangumi_list'
 	allowed_domains = [bangumi_settings.BASE_DOMAIN]
 	start_page = 1
-	last_page = 1
+	last_page = None
 
 	def __init__(self, **kwargs):
 		self.update_properties()
@@ -20,12 +19,14 @@ class BangumiListSpider(scrapy.Spider):
 		cur_url = self.get_current_url()
 		first_page_html = requests.get(cur_url,headers=bangumi_settings.HEADERS).content.decode('utf-8')
 		self.last_page=int(re.findall('page=[0-9][0-9]*',first_page_html)[-1][5:])
-		#print("last_page:",self.last_page)
+		print("last_page:",self.last_page)
 	
 	def update_properties(self):
 		self.page_var = self.start_page
 		self.get_last_page()
-		self.start_urls = [self.get_url_from_params(_page=pg) for pg in range(1,self.last_page+1)]
+		if self.last_page:
+			self.start_urls = [self.get_url_from_params(_page=pg) for pg in range(1,self.last_page+1)]
+		else: self.start_urls=[self.get_current_url()]
 
 	def get_url_from_params(self,_page=None,_type=None):
 		_type = self.type if not _type else _type
@@ -50,5 +51,7 @@ class BangumiListSpider(scrapy.Spider):
 			result['name'] = item.xpath('./div/h3/small[@class="grey"]/text()').get()
 			if result['name'] == None: result['name'] = result['cn_name']
 			yield result
-		#self.page_var += 1
-		#yield scrapy.Request(self.get_current_url())
+
+		if not self.last_page:
+			self.page_var += 1
+			yield scrapy.Request(self.get_current_url())
