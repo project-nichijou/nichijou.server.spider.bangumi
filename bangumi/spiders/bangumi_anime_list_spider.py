12759fa38e746c4d24f2af2bab7d1f01a4743d62
JeffersonQin <1247006353@qq.com>
2021-07-13 00:26:39 +0800

    [Update] anime list spiders

--- bangumi/spiders/bangumi_anime_list_spider.py
+++ bangumi/spiders/bangumi_anime_list_spider.py
@@ -1,9 +1,9 @@
 import scrapy
-from bangumi.spiders.bangumi_list import BangumiListSpider
+from bangumi.spiders.bangumi_list_spider import BangumiListSpider
 
 
 class BangumiAnimeListSpider(BangumiListSpider):
+
 	name = 'bangumi_anime_list'
 	type = 'anime'
 	start_page = 1
-
--- bangumi/spiders/bangumi_list_spider.py
+++ bangumi/spiders/bangumi_list_spider.py
@@ -1,22 +1,37 @@
-from bangumi.tools import bangumi_cookies
-from bangumi.items.bangumi_log import BangumiLogItem
-from bangumi.config import bangumi_settings
+from common.utils.logger import format_log
+from common.utils.datetime import get_time_str_now
+from common.items.log_item import CommonLogItem
 from bangumi.items.bangumi_id import BangumiIDItem
-import scrapy
+from bangumi.config import bangumi_settings
+from common.spiders.common_spider import CommonSpider
+from common.config import settings as common_settings
+
 import re
 import requests
+import scrapy
 import traceback
 
 
-class BangumiListSpider(scrapy.Spider):
+class BangumiListSpider(CommonSpider):
+	
+	# scrapy
 	name = 'bangumi_list'
 	allowed_domains = [bangumi_settings.BASE_DOMAIN]
+
+	# common
+	use_cookies = False
+	
+	# private
 	start_page = 1
 	last_page = None
 	page_count = {}
 	sid_list = []
 
+
 	def __init__(self, **kwargs):
+		'''
+		initializer for spider
+		'''
 		self.page_var = self.start_page
 		self.get_last_page()
 		if self.last_page:
@@ -25,32 +40,34 @@ class BangumiListSpider(scrapy.Spider):
 			]
 		else: self.start_values = [{'url': self.get_url(), 'page': self.page_var}]
 		super().__init__(**kwargs)
-	
+
+
 	def get_last_page(self):
+		'''
+		get the number of last page to confirm scrape range
+		'''
 		cur_url = self.get_url()
-		first_page_html = requests.get(cur_url, headers=bangumi_settings.HEADERS).content.decode('utf-8')
+		first_page_html = requests.get(cur_url, headers=common_settings.HEADERS).content.decode('utf-8')
 		self.last_page = int(re.findall('page=[0-9][0-9]*', first_page_html)[-1][5:])
 		print("last_page:", self.last_page)
 
-	def request(self, url, callback, errback, cb_kwargs=None):
-		'''
-		warpper for scrapy.request
-		'''
-		request = scrapy.Request(url=url, callback=callback, errback=errback, cb_kwargs=cb_kwargs)
-		request.cookies = bangumi_cookies.read_cookies()
-		headers = bangumi_settings.HEADERS
-		for key in headers.keys():
-			request.headers[key] = headers[key]
-		return request
-	
-	def start_requests(self):
-		for _, val in enumerate(self.start_values):
-			yield self.request(url=val['url'], callback=self.parse, errback=self.errback, cb_kwargs={'page': val['page']})
 
 	def get_url(self, page=None, type=None):
+		'''
+		generate url used for scraping
+		'''
 		type = self.type if not type else type
 		page = self.page_var if not page else page
 		return f'{bangumi_settings.BASE_URL}/{type}/browser/?sort=title&page={page}'
+
+
+	def start_requests(self):
+		'''
+		scraping entrance
+		'''
+		for _, val in enumerate(self.start_values):
+			yield self.request(url=val['url'], callback=self.parse, errback=self.errback, cb_kwargs={'page': val['page']})
+
 
 	def parse(self, response, page):
 		# Update cookies
@@ -74,42 +91,50 @@ class BangumiListSpider(scrapy.Spider):
 				result['name_cn'] = item.xpath('./div/h3/a/text()').get()
 				result['name'] = item.xpath('./div/h3/small[@class="grey"]/text()').get()
 				if result['name'] == None: result['name'] = result['name_cn']
+				
 				### logging section
 				self.page_count[str(page)] += 1
 				
 				if result['sid'] in self.sid_list:
-					yield BangumiLogItem.create(content=(
-						'duplicate sid detected! (in bangumi_list) \n'
-						f' type: {self.type} \n'
-						f' page: {page} \n'
-						f' sid: {result["sid"]} \n'
-						f' name: {result["name"]} \n'
-						f' name_cn: {result["name_cn"]}'
-					))
+					log_dict = dict(result)
+					log_dict['page'] = page
+					yield CommonLogItem(
+						time = get_time_str_now(),
+						content = format_log(
+							info = 'duplicate `sid` detected! (in `bangumi_list`)',
+							values = log_dict
+						)
+					)
 				else: self.sid_list.append(result['sid'])
-				###
+				
 				yield result
 			except Exception as e:
-				log_res = BangumiLogItem.create(content=(
-					f'exception caught in bangumi_list \n'
-					f' type: {self.type} \n'
-					f' page: {page} \n'
-					f' sid: {result["sid"]} \n'
-					f' exception info: {repr(e)} \n'
-					f' traceback: \n'
-					f' {traceback.format_exc()}'
-				))
-				yield log_res
+				log_dict = dict(result)
+				log_dict['page'] = page
+				yield CommonLogItem(
+					time = get_time_str_now(),
+					content = format_log(
+						info = 'exception caught in `bangumi_list`',
+						exception = e,
+						traceback = traceback.format_exc(),
+						values = log_dict
+					)
+				)
 		
 		if self.page_count[str(page)] < 24:
-			yield BangumiLogItem.create(content=(
-				'list count warning! (in bangumi_list) \n'
-				f' page: {page} \n'
-				f' type: {self.type} \n'
-				f' count: {self.page_count[str(page)]} \n'
-				f' last_page: {self.last_page} \n'
-				f' url: {self.get_url(page=page)}'
-			))
+			yield CommonLogItem(
+				time = get_time_str_now(),
+				content = format_log(
+					info = 'list count warning! (in bangumi_list)',
+					values = {
+						'page': page,
+						'type': self.type,
+						'count': self.page_count[str(page)],
+						'last_page': self.last_page,
+						'url': self.get_url(page=page)
+					}
+				)
+			)
 
 		if page == self.last_page:
 			self.last_page = None
@@ -118,13 +143,3 @@ class BangumiListSpider(scrapy.Spider):
 		if not self.last_page:
 			self.page_var += 1
 			yield self.request(url=self.get_url(), callback=self.parse, errback=self.errback, cb_kwargs={'page': self.page_var})
-
-	def errback(self, failure):
-		page = failure.request.cb_kwargs['page']
-		yield BangumiLogItem.create(content=(
-			f'exception caught in errback of bangumi_list: \n'
-			f' {repr(failure)} \n'
-			f' traceback: \n'
-			f' {failure.getTraceback()} \n'
-			f' page: {page}'
-		))
